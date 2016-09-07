2f1cba3963bd248f59d2c08b2991c7c4d0885b79
David Chung <david.chung@docker.com>
2016-09-07 15:32:34 -0700

    Update schema

--- types/infra/config.go
+++ types/infra/config.go
@@ -1,17 +1,37 @@
+// +build experimental
+
 package infra
 
 import (
 	"encoding/json"
 )
 
+// URLString is a URL string
+type URLString string
+
 // Config defines a SWIM config
 type Config struct {
-	Groups []Group `json:",omitempty"`
+	// Groups is a map keyed by the name of the group
+	Groups map[string]Group `json:",omitempty"`
+
+	// Addresses is an association of url string to addresses
+	Addresses map[URLString]Address `json:",omitempty"`
 }
 
 // Group defines a scale group
 type Group struct {
-	Name       string           `json:",omitempty"`
-	Driver     *string          `json:",omitempty"`
+
+	// Plugin is the name of the plugin (user/repo) that implements a GroupDriver interface.
+	Plugin *string `json:",omitempty"`
+
+	// Properties capture the free-form properties for the plugin GroupDriver
 	Properties *json.RawMessage `json:",omitempty"`
+}
+
+// Addresse models an endpoint for the services running in a swarm
+type Address struct {
+	// Service is the swarm service name
+	Service string `json:",omitempty"`
+
+	// TODO - add L4, L7 load balancer configs
 }
